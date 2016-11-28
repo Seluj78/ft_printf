0a8586ab16f68e016bc7939fcb6a704887aa9ce2
Jules LASNE <jlasne@student.42.fr>
2016-11-28 15:04:05 +0100

    Fixed the main test

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jlasne <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/15 19:22:28 by jlasne            #+#    #+#             */
-/*   Updated: 2016/11/28 15:00:46 by jlasne           ###   ########.fr       */
+/*   Updated: 2016/11/28 15:01:48 by jlasne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@ int		main(int argc, char **argv)
 	(void)argv;
 	ft_printf("String: %s\nDecimal: %d\n", "This is a String", 42);
 	ft_printf("Char: %c\nPointer adress: %p\n", 'c', "String adress");
-	ft_printf("downcase hex: %x\nUPCASE HEX: %x\n", 123456789, 123456789);
+	ft_printf("downcase hex: %x\nUPCASE HEX: %X\n", 123456789, 123456789);
 	return (0);
 }
