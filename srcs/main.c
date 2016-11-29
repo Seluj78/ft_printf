cf293ad2f9577efa354ef4580b2683fb4e417652
jlasne <jlasne@student.42.fr>
2016-11-29 17:23:23 +0100

    Got the parsing workinh

--- srcs/main.c
+++ srcs/main.c
@@ -16,21 +16,17 @@ int		main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
-	ft_printf("%d", 424242);
-//	ft_printf("String: %s\nDecimal: %d\n", "This is a String", 42);
-	/*ft_printf("Char: %c\nPointer adress: %p\n", 'c', "String adress");
+	ft_printf("String: %s\nDecimal: %d\n", "This is a String", 42);
+	ft_printf("Char: %c\nPointer adress: %p\n", 'c', "String adress");
 	ft_printf("downcase hex: %x\nUPCASE HEX: %X\n", 123456789, 123456789);
 	ft_printf("Unsigned decimal: %u ; %u\n", 42, -42);
 	ft_printf("Octal convertion: %o\n", 424242424242);
 	ft_putstr("\n\n");
-
-*/
-	//ft_putstr("\n\n");
-	//printf("String: %s\nDecimal: %d\n", "This is a String", 42);
-	/*printf("Char: %c\nPointer adress: %p\n", 'c', "String adress");
+	ft_putstr("\n\n");
+	printf("String: %s\nDecimal: %d\n", "This is a String", 42);
+	printf("Char: %c\nPointer adress: %p\n", 'c', "String adress");
 	printf("downcase hex: %x\nUPCASE HEX: %X\n", 123456789, 123456789);
 	printf("Unsigned decimal: %u ; %u\n", 42, -42);
 	ft_printf("Octal convertion: %o\n", 424242424242);
-	*/
 	return (0);
 }
