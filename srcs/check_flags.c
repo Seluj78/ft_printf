2a241d3336632edf3ac6dae80cf09438d44e0c4c
Elliott STEPHAN <estephan@e1r13p14.42.fr>
2016-12-05 11:19:14 +0100

    Default Quick Commit

--- srcs/check_flags.c
+++ srcs/check_flags.c
@@ -6,7 +6,7 @@
 /*   By: estephan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/01 14:30:56 by estephan          #+#    #+#             */
-/*   Updated: 2016/12/02 17:14:09 by estephan         ###   ########.fr       */
+/*   Updated: 2016/12/05 11:18:51 by estephan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,12 +54,14 @@ void	check_precision(t_data *data, int	nb)
 
 	i = 0;
 	k = 0;
-	s = 0;
+	s = -1;
 	while (data->conv[i] != '\0' && k == 0)
 	{
 		if (data->conv[i] == '.')
 		{
 			i++;
+			if (data->conv[i] == '*')
+				s = nb;
 			while (data->conv[i] >= '0' && data->conv[i] <= '9')
 			{
 				i++;
@@ -68,26 +70,32 @@ void	check_precision(t_data *data, int	nb)
 		}
 		i++;
 	}
-	str = ft_strnew(k);
-	i = 0;
-	k = 0;
-	while (data->conv[i] != '\0' && k == 0)
+	if (s != -1)
 	{
-		if (data->conv[i] == '.')
+		str = ft_strnew(k);
+		i = 0;
+		k = 0;
+		s = 0;
+		while (data->conv[i] != '\0' && k == 0)
 		{
-			i++;
-			while (data->conv[i] >= '0' && data->conv[i] <= '9')
+			if (data->conv[i] == '.')
 			{
-				str[s] = data->conv[i];
-				s++;	
 				i++;
-				k++;
+				while (data->conv[i] >= '0' && data->conv[i] <= '9')
+				{
+					str[s] = data->conv[i];
+					s++;	
+					i++;
+					k++;
+				}
 			}
+			i++;
 		}
-		i++;
+		str[s] = '\0';
+		s = ft_atoi(str);
 	}
-	str[s] = '\0';
-	s = ft_atoi(str);
+	else
+		nb = va_arg(*data->ap, int);
 	while (s > ft_nblen(nb))
 	{
 		data->ret += write(1, "0", 1);
