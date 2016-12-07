7c0e4f9b1bf05d73cd60a0bff20f8a241beae636
jlasne <jlasne@student.42.fr>
2016-12-07 13:34:59 +0100

    Default Quick Commit

--- Makefile
+++ Makefile
@@ -32,19 +32,19 @@ OBJ_NAME = $(SRC_NAME:.c=.o)
 SRC = $(addprefix $(SRCDIR)/,$(SRC_NAME))
 OBJ = $(addprefix $(OBJDIR)/,$(OBJ_NAME))
 
-#.SILENT:
+.SILENT:
 
 all: $(NAME)
-	#echo "\033[38;5;44m☑️  ALL    $(NAMEBASE) is done\033[0m\033[K"
+	echo "\033[38;5;44m☑️  ALL    $(NAMEBASE) is done\033[0m\033[K"
 
 $(NAME): $(OBJ) $(LIB)
-	#printf "\r\033[38;5;11m⌛  MAKE   $(NAMEBASE) please wait ...\033[0m\033[K"
+	printf "\r\033[38;5;11m⌛  MAKE   $(NAMEBASE) please wait ...\033[0m\033[K"
 	cp $(LIB) $(NAME)
 	ar rc $(NAME) $(OBJ)
 	ranlib $(NAME)
-	#echo -en "\r\033[38;5;22m☑️  MAKE   $(NAMEBASE)\033[0m\033[K"
-	#echo "\r\033[38;5;184m👥  GROUP MEMBER(S):\033[0m\033[K"
-	#echo "\r\033[38;5;15m`cat auteur | sed s/^/\ \ \ \ /g`\033[0m\033[K"
+	echo -en "\r\033[38;5;22m☑️  MAKE   $(NAMEBASE)\033[0m\033[K"
+	echo "\r\033[38;5;184m👥  GROUP MEMBER(S):\033[0m\033[K"
+	echo "\r\033[38;5;15m`cat auteur | sed s/^/\ \ \ \ /g`\033[0m\033[K"
 
 $(OBJ): $(OBJDIR)/%.o: $(SRCDIR)/%.c
 	mkdir -p $(OBJDIR)
@@ -55,17 +55,17 @@ $(LIB):
 	make -C libft/
 
 clean:
-	#printf "\r\033[38;5;11m⌛  CLEAN  $(NAMEBASE) please wait ...\033[0m\033[K"
+	printf "\r\033[38;5;11m⌛  CLEAN  $(NAMEBASE) please wait ...\033[0m\033[K"
 	rm -f $(OBJ)
 	rm -f $(OBJDIR)/$(OBJ_NAME)
 	make -C libft/ clean
-	#printf "\r\033[38;5;11m☑️  CLEAN  $(NAMEBASE) is done\033[0m\033[K"
+	printf "\r\033[38;5;11m☑️  CLEAN  $(NAMEBASE) is done\033[0m\033[K"
 
 fclean: clean
-	#printf "\r\033[38;5;11m⌛  FCLEAN $(NAMEBASE) please wait ...\033[0m\033[K"
+	printf "\r\033[38;5;11m⌛  FCLEAN $(NAMEBASE) please wait ...\033[0m\033[K"
 	rm -f $(NAME)
 	make -C libft/ fclean
-	#printf "\r\033[38;5;11m☑️  FCLEAN  $(NAMEBASE) is done\033[0m\033[K"
+	printf "\r\033[38;5;11m☑️  FCLEAN  $(NAMEBASE) is done\033[0m\033[K"
 
 re: fclean all
 
--- includes/struct.h
+++ includes/struct.h
@@ -6,7 +6,7 @@
 /*   By: jlasne <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/15 19:24:20 by jlasne            #+#    #+#             */
-/*   Updated: 2016/12/07 13:16:18 by jlasne           ###   ########.fr       */
+/*   Updated: 2016/12/07 13:33:01 by jlasne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,51 +57,47 @@ typedef struct	s_data
 /*
 ** COLORS
 */
-/*
-[reset] =\033[0m
 
+# define RESET = "\033[0m"
 
-//Regular Colors
-
-[Black]=\033[0;30m        # Black
-[Red]=\033[0;31m          # Red
-[Green]=\033[0;32m        # Green
-[Yellow]=\033[0;33m       # Yellow
-[Blue]=\033[0;34m         # Blue
-[Purple]=\033[0;35m       # Purple
-[Cyan]=\033[0;36m         # Cyan
-[White]=\033[0;37m        # White
-
-
-//Bold
-
-[BBlack]=\033[1;30m       # Black
-[BRed]=\033[1;31m         # Red
-[BGreen]=\033[1;32m       # Green
-[BYellow]=\033[1;33m      # Yellow
-[BBlue]=\033[1;34m        # Blue
-[BPurple]=\033[1;35m      # Purple
-[BCyan]=\033[1;36m        # Cyan
-[BWhite]=\033[1;37m       # White
-
-
-//Underline
-
-[UBlack]=\033[4;30m       # Black
-[URed]=\033[4;31m         # Red
-[UGreen]=\033[4;32m       # Green
-[UYellow]=\033[4;33m      # Yellow
-[UBlue]=\033[4;34m        # Blue
-[UPurple]=\033[4;35m      # Purple
-[UCyan]=\033[4;36m        # Cyan
-[UWhite]=\033[4;37m       # White
-*/
 
 /*
-** va_list *ap -> Args list used by ft_printf
-** l_format -> format length
-** format -> contains the XXX in  ft_printf("XXX", foo, bar);
-** TODO ->#+-0' 'taille min champ, precision,
-** current_percent -> current percent being processed
+** Regular Colors
 */
+
+# define BLACK = "\033[0;30m"
+# define RED = "\033[0;31m"
+# define GREEN = "\033[0;32m"
+# define YELLOW = "\033[0;33m"
+# define BLUE = "\033[0;34m"
+# define PURPLE = "\033[0;35m"
+# define CYAN = "\033[0;36m"
+# define WHITE = "\033[0;37m"
+
+/*
+** Blold
+*/
+
+# define BBLACK = "\033[1;30m"
+# define BRED = "\033[1;31m"
+# define BGREEN = "\033[1;32m"
+# define BYELLOW = "\033[1;33m"
+# define BBLUE = "\033[1;34m"
+# define BPURPLE = "\033[1;35m"
+# define BCYAN = "\033[1;36m"
+# define BWHITE = "\033[1;37m"
+
+/*
+** Underline
+*/
+
+# define UBLACK = "\033[4;30m"
+# define URED = "\033[4;31m"
+# define UGREEN = "\033[4;32m"
+# define UYELLOW = "\033[4;33m"
+# define UBLUE = "\033[4;34m"
+# define UPURPlE = "\033[4;35m"
+# define UCYAN = "\033[4;36m"
+# define UWHITE = "\033[4;37m"
+
 #endif
