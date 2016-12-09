1503611450c0b2b61a4a7dd9857c8944b3707f04
jlasne <jlasne@student.42.fr>
2016-12-09 23:31:40 +0100

    Added ALL THE COLORS

--- srcs/color.c
+++ srcs/color.c
@@ -47,7 +47,103 @@ int		put_color(t_data *data, int i)
 		ft_putstr(WHITE);
 		return (8);
 	}
-	else {}
+	
+
+
+
+
+
+
+
+
+	else if (!ft_strncmp(&data->format[data->index], "{:bblack}", i))
+	{
+		ft_putstr(BBLACK);
+		return (9);
+	}
+	else if (!ft_strncmp(&data->format[data->index], "{:bred}", i))
+	{
+		ft_putstr(BRED);
+		return (7);
+	}
+	else if (!ft_strncmp(&data->format[data->index], "{:bgreen}", i))
+	{
+		ft_putstr(BGREEN);
+		return (9);
+	}
+	else if (!ft_strncmp(&data->format[data->index], "{:byellow}", i))
+	{
+		ft_putstr(BYELLOW);
+		return (10);
+	}
+	else if (!ft_strncmp(&data->format[data->index], "{:bblue}", i))
+	{
+		ft_putstr(BBLUE);
+		return (8);
+	}
+	else if (!ft_strncmp(&data->format[data->index], "{:bpurple}", i))
+	{
+		ft_putstr(BPURPLE);
+		return (10);
+	}
+	else if (!ft_strncmp(&data->format[data->index], "{:bcyan}", i))
+	{
+		ft_putstr(BCYAN);
+		return (8);
+	}
+	else if (!ft_strncmp(&data->format[data->index], "{:bwhite}", i))
+	{
+		ft_putstr(BWHITE);
+		return (9);
+	}
+
+
+
+
+
+
+	else if (!ft_strncmp(&data->format[data->index], "{:ublack}", i))
+	{
+		ft_putstr(UBLACK);
+		return (9);
+	}
+	else if (!ft_strncmp(&data->format[data->index], "{:ured}", i))
+	{
+		ft_putstr(URED);
+		return (7);
+	}
+	else if (!ft_strncmp(&data->format[data->index], "{:ugreen}", i))
+	{
+		ft_putstr(UGREEN);
+		return (9);
+	}
+	else if (!ft_strncmp(&data->format[data->index], "{:uyellow}", i))
+	{
+		ft_putstr(UYELLOW);
+		return (10);
+	}
+	else if (!ft_strncmp(&data->format[data->index], "{:ublue}", i))
+	{
+		ft_putstr(UBLUE);
+		return (8);
+	}
+	else if (!ft_strncmp(&data->format[data->index], "{:upurple}", i))
+	{
+		ft_putstr(UPURPLE);
+		return (10);
+	}
+	else if (!ft_strncmp(&data->format[data->index], "{:ucyan}", i))
+	{
+		ft_putstr(UCYAN);
+		return (8);
+	}
+	else if (!ft_strncmp(&data->format[data->index], "{:uwhite}", i))
+	{
+		ft_putstr(UWHITE);
+		return (9);
+	}
+
+
 	return (0);
 }
 
