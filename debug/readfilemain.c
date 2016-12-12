3227500061c238c5ddfda5f2526f33b471a59279
jlasne <jlasne@student.42.fr>
2016-12-12 14:38:43 +0100

    Default Quick Commit

--- debug/readfilemain.c
+++ debug/readfilemain.c
@@ -6,7 +6,7 @@
 /*   By: jlasne <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/12 14:24:56 by jlasne            #+#    #+#             */
-/*   Updated: 2016/12/12 14:24:58 by jlasne           ###   ########.fr       */
+/*   Updated: 2016/12/12 14:36:21 by jlasne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 
 int		main(int argc, char **argv)
 {
-	ft_printf("%r\n\n\n\n\n\n%r", "testfile.txt", "testfile2.txt");
+	//ft_printf("%r\n\n\n\n\n\n%r", "testfile.txt", "testfile2.txt");
+	ft_printf("%d", 42);
 }
